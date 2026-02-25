20240cba15c48e1a4b86f11332ffb72999396dd9
Monish-2007 <k2007monish@gmail.com>
2026-02-25 10:28:49 +0530

    UC3: Reverse string based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,6 +3,7 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         showWelcome();   // UC1
         checkHardcodedPalindrome();  // UC2
+        reverseBasedPalindromeCheck(); // UC3
     }
 
     // UC1: Welcome Message
@@ -33,6 +34,29 @@ public class PalindromeCheckerApp {
 
         System.out.println("Input text: " + input);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+
+    // UC3: Reverse String Based Palindrome Check
+    public static void reverseBasedPalindromeCheck() {
+
+        String input = "madam";
+        String reversed = "";
+
+        // Reverse using loop (as per your hint)
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        System.out.println("UC3 Result");
+        System.out.println("Input text: " + input);
+
+        if (input.equals(reversed)) {
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Is it a Palindrome? : false");
+        }
+
+        System.out.println();
     }
 }
 
