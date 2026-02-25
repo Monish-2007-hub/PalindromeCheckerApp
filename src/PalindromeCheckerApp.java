ff2f14992cddd9a29bb41eab76afbab55edde531
Monish-2007 <k2007monish@gmail.com>
2026-02-25 10:09:01 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,6 +2,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         showWelcome();   // UC1
+        checkHardcodedPalindrome();  // UC2
     }
 
     // UC1: Welcome Message
@@ -14,6 +15,24 @@ public class PalindromeCheckerApp {
 
         // Continue to next use case (placeholder)
         System.out.println("System initialized successfully.");
+    }
+
+    // UC2: Hardcoded Palindrome Check
+    public static void checkHardcodedPalindrome() {
+
+        String input = "madam";   // Hardcoded string
+        boolean isPalindrome = true;
+
+        // Loop till half of the string
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
 
