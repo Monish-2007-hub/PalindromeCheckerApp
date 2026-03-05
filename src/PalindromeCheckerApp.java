cfa19e6eb0ec1f7ef9c4609266bb6ec80394b514
Monish-2007-hub <k2007monish@gmail.com>
2026-03-05 10:20:23 +0530

    Added UC10 normalized palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -15,6 +15,7 @@ public class PalindromeCheckerApp {
         dequePalindromeCheck();   // UC7
         linkedListPalindromeCheck();   // UC8
         recursivePalindromeCheck();   // UC9
+        normalizedPalindromeCheck(); // UC10
     }
 
     // UC1: Welcome Message
@@ -268,6 +269,28 @@ public class PalindromeCheckerApp {
         // Recursive call
         return check(s, start + 1, end - 1);
     }
+    // UC10: Case-Insensitive & Space-Ignored Palindrome
+    public static void normalizedPalindromeCheck() {
+
+        String input = "A man a plan a canal Panama";
+
+        // Normalize the string (remove spaces/symbols and convert to lowercase)
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("UC10 Result");
+        System.out.println("Input : " + input);
+        System.out.println("Normalized : " + normalized);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
-
-
