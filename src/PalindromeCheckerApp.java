2cf3b246c469135ae97f36b6be5f07adcb0f344c
Monish-2007-hub <k2007monish@gmail.com>
2026-03-05 09:22:59 +0530

    Added UC8 LinkedList Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -13,6 +13,7 @@ public class PalindromeCheckerApp {
         stackBasedPalindromeCheck();   // UC5
         queueStackPalindromeCheck();   // UC6
         dequePalindromeCheck();   // UC7
+        linkedListPalindromeCheck();   // UC8
     }
 
     // UC1: Welcome Message
@@ -199,6 +200,41 @@ public class PalindromeCheckerApp {
 
         // Display result
         System.out.println("UC7 Result");
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println();
+    }
+    // UC8: Linked List Based Palindrome Checker
+    public static void linkedListPalindromeCheck() {
+
+        // Define input string
+        String input = "level";
+
+        // Create LinkedList
+        LinkedList<Character> list = new LinkedList<>();
+
+        // Add characters to linked list
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Compare first and last elements
+        while (list.size() > 1) {
+
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("UC8 Result");
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
         System.out.println();
