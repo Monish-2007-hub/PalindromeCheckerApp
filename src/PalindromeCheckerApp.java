46902430c7619a1ae6ec7abc55d180d214e12abb
Monish-2007-hub <k2007monish@gmail.com>
2026-03-05 08:58:59 +0530

    Added UC6 Queue + Stack Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class PalindromeCheckerApp {
         reverseBasedPalindromeCheck(); // UC3
         characterArrayPalindromeCheck();   // UC4
         stackBasedPalindromeCheck();   // UC5
+        queueStackPalindromeCheck();   // UC6
     }
 
     // UC1: Welcome Message
@@ -120,6 +123,44 @@ public class PalindromeCheckerApp {
 
         // Display result
         System.out.println("UC5 Result");
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println();
+    }
+    // UC6: Queue + Stack Based Palindrome Check
+    public static void queueStackPalindromeCheck() {
+
+        // Define the input string
+        String input = "civic";
+
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Create Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
+
+        // Insert characters into queue and stack
+        for (char c : input.toCharArray()) {
+            queue.add(c);   // enqueue
+            stack.push(c);  // push
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue and pop
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove(); // dequeue
+            char fromStack = stack.pop();    // pop
+
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("UC6 Result");
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
         System.out.println();
