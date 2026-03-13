26511bb97d1ea249d92e4e5bb89960701625c8f4
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:10:59 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,30 +1,21 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        normalizedPalindromeCheck(); // UC10
-    }
-    // UC10: Case-Insensitive & Space-Ignored Palindrome
-    public static void normalizedPalindromeCheck() {
 
         String input = "A man a plan a canal Panama";
 
-        // Normalize the string (remove spaces/symbols and convert to lowercase)
         String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         boolean isPalindrome = true;
 
-        // Compare characters from both ends
         for (int i = 0; i < normalized.length() / 2; i++) {
-
             if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        System.out.println("UC10 Result");
-        System.out.println("Input : " + input);
-        System.out.println("Normalized : " + normalized);
+        System.out.println("Input: " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
