3b24d5a500b188f712db0768e8e629b895b5c651
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:04:31 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,10 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        checkHardcodedPalindrome();  // UC2
-    }
-    // UC2: Hardcoded Palindrome Check
-    public static void checkHardcodedPalindrome() {
 
-        String input = "madam";   // Hardcoded string
+        String input = "madam";
         boolean isPalindrome = true;
 
-        // Loop till half of the string
         for (int i = 0; i < input.length() / 2; i++) {
             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
                 isPalindrome = false;
@@ -17,8 +12,8 @@ public class PalindromeCheckerApp {
             }
         }
 
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
 
