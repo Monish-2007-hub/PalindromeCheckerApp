e334516ffc2f8d41be9e1c3af95bc703734feacc
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:05:58 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,14 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        characterArrayPalindromeCheck();   // UC4
-    }
-    // UC4: Character Array Based Palindrome Check
-    public static void characterArrayPalindromeCheck() {
 
-        // Declare and initialize input string
         String input = "radar";
-
-        // Convert string into character array
         char[] chars = input.toCharArray();
 
-        // Initialize pointers
         int start = 0;
         int end = chars.length - 1;
-
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Compare characters using two-pointer approach
         while (start < end) {
             if (chars[start] != chars[end]) {
                 isPalindrome = false;
@@ -29,10 +18,8 @@ public class PalindromeCheckerApp {
             end--;
         }
 
-        // Display result
-        System.out.println("Input : " + input);
+        System.out.println("Input: " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-        System.out.println();
     }
 }
 
