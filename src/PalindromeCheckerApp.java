452e15dcfa51f79acf746e407f3770d944b2cdd6
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:03:47 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,9 @@
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-        showWelcome();   // UC1
-    }
+     public static void main(String[] args) {
 
-    // UC1: Welcome Message
-    public static void showWelcome() {
-        // Display welcome message
-        System.out.println("welcome to the Palindrome Checker Management System");
-
-        // Display version
+        System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
-
-        // Continue to next use case (placeholder)
         System.out.println("System initialized successfully.");
     }
 }
