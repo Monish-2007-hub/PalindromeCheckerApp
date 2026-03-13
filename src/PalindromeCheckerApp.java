b57e841b27aa28ba40e55c7d27b8e01140a3ea28
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:08:45 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,45 +1,26 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.*;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        dequePalindromeCheck();   // UC7
-    }
-    // UC7: Deque Based Optimized Palindrome Checker
-    public static void dequePalindromeCheck() {
 
-        // Define input string
         String input = "refer";
-
-        // Create Deque
         Deque<Character> deque = new ArrayDeque<>();
 
-        // Add each character into deque
         for (char c : input.toCharArray()) {
             deque.addLast(c);
         }
 
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Compare front and rear elements
         while (deque.size() > 1) {
-
-            char first = deque.removeFirst();
-            char last = deque.removeLast();
-
-            if (first != last) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Display result
-        System.out.println("UC7 Result");
-        System.out.println("Input : " + input);
+        System.out.println("Input: " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-        System.out.println();
     }
 }
-
-
