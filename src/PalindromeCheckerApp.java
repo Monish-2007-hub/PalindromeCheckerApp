5ef9d71a11bddfe3c94a6b5ddaa23137ee1145ce
Monish-2007-hub <k2007monish@gmail.com>
2026-03-13 18:08:06 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,49 +1,29 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        queueStackPalindromeCheck();   // UC6
-    }
-    // UC6: Queue + Stack Based Palindrome Check
-    public static void queueStackPalindromeCheck() {
 
-        // Define the input string
         String input = "civic";
 
-        // Create Queue (FIFO)
         Queue<Character> queue = new LinkedList<>();
-
-        // Create Stack (LIFO)
         Stack<Character> stack = new Stack<>();
 
-        // Insert characters into queue and stack
         for (char c : input.toCharArray()) {
-            queue.add(c);   // enqueue
-            stack.push(c);  // push
+            queue.add(c);
+            stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue and pop
         while (!queue.isEmpty()) {
-
-            char fromQueue = queue.remove(); // dequeue
-            char fromStack = stack.pop();    // pop
-
-            if (fromQueue != fromStack) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Display result
-        System.out.println("UC6 Result");
-        System.out.println("Input : " + input);
+        System.out.println("Input: " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-        System.out.println();
     }
 }
-
-
